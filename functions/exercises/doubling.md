239d9a43d9d55629a3dca5752c1388e7a3c7655c
tminus109 <martontibor123@gmail.com>
2021-11-09 15:32:06 +0100

    completed Doubling exercise

---
+++ functions/exercises/doubling.md
@@ -0,0 +1,3 @@
+Create an integer variable named `baseNumber` and assign the value `123` to it.
+Create a function called `doubleNumber()` that doubles its integer input parameter and returns the doubled value.
+Print the result of `doubleNumber(baseNumber)`.
---
+++ functions/src/Doubling.java
@@ -0,0 +1,10 @@
+public class Doubling {
+    public static int doubler(int baseNumber) {
+        return baseNumber * 2;
+    }
+
+    public static void main(String[] args) {
+        int baseNumber = 123;
+        System.out.println(doubler(baseNumber));
+    }
+}
