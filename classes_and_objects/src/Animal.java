674de01476f6c155f64647778e5313969a156a74
tminus109 <martontibor123@gmail.com>
2022-01-24 10:10:44 +0100

    Animal exercise completed

---
+++ classes_and_objects/src/Animal.java
@@ -0,0 +1,21 @@
+public class Animal {
+    int hunger, thirst;
+
+    Animal() {
+        this.hunger = 50;
+        this.thirst = 50;
+    }
+
+    public void eat() {
+        this.hunger++;
+    }
+
+    public void drink() {
+        this.thirst++;
+    }
+
+    public void play() {
+        this.hunger--;
+        this.thirst--;
+    }
+}
