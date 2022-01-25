a4a73c197f547de5b7ef0ff3c8b4bddbe723d6b4
tminus109 <martontibor123@gmail.com>
2022-01-25 19:47:34 +0100

    Fibonacci exercise completed

---
+++ recursion/src/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    static int fibonacci(int n) {
+        if (n < 0) {
+            return 0;
+        } else if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+    }
+}
