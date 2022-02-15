9dec09bdddceb1a195f4208322b2bb12004ef8e0
tminus109 <martontibor123@gmail.com>
2022-02-15 13:30:19 +0100

    Exercise 2 complete

--- streams/src/StreamUtils.java
+++ streams/src/StreamUtils.java
@@ -3,15 +3,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamUtils {
-
     public static List<Integer> findEvenNumbers(List<Integer> numbers) {
         return numbers.stream()
                 .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
     }
 
+    public static List<Integer> squarePositiveNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n > 0)
+                .map(n -> n * n)
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
         System.out.println(findEvenNumbers(numbers));
+        System.out.println(squarePositiveNumbers(numbers));
     }
 }
