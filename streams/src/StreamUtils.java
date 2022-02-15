f9996cf21e718525be4810cfe912ff107a02a79c
tminus109 <martontibor123@gmail.com>
2022-02-15 13:34:52 +0100

    Exercise 3 complete

--- streams/src/StreamUtils.java
+++ streams/src/StreamUtils.java
@@ -16,10 +16,19 @@ public class StreamUtils {
                 .collect(Collectors.toList());
     }
 
+    public static List<Integer> findNumbersSquaredGreaterThan20(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n * n > 20)
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-
         System.out.println(findEvenNumbers(numbers));
+
         System.out.println(squarePositiveNumbers(numbers));
+
+        List<Integer> numbers2 = Arrays.asList(3, 9, 2, 8, 6, 5);
+        System.out.println(findNumbersSquaredGreaterThan20(numbers2));
     }
 }
