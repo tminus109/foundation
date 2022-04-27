1db4ace4121868f44314ff8a3498d55e4e857717
tminus109 <martontibor123@gmail.com>
2022-04-27 10:46:52 +0200

    feat: spring initializr, dependencies

---
+++ p2p_chat/src/main/java/com/gfa/p2p_chat/P2pChatApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.p2p_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2pChatApplication.class, args);
+	}
+
+}
---
+++ p2p_chat/src/test/java/com/gfa/p2p_chat/P2pChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfa.p2p_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2pChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
