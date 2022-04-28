b10bbb48d965bca59f7f898e8fe87fd823d3c8ee
tminus109 <martontibor123@gmail.com>
2022-04-28 09:33:42 +0200

    feat: Log

--- p2p_chat/src/main/java/com/gfa/p2p_chat/controllers/MainController.java
+++ p2p_chat/src/main/java/com/gfa/p2p_chat/controllers/MainController.java
@@ -1,4 +1,13 @@
 package com.gfa.p2p_chat.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String showIndex() {
+        return "index";
+    }
 }
--- p2p_chat/src/main/java/com/gfa/p2p_chat/models/Log.java
+++ p2p_chat/src/main/java/com/gfa/p2p_chat/models/Log.java
@@ -1,4 +1,40 @@
 package com.gfa.p2p_chat.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Log {
+    @Id
+    @GeneratedValue
+    private long id;
+    private LocalDateTime dateTime;
+    private String logLevel;
+    private String path;
+    private String method;
+    private String requestData;
+
+    public Log() {
+        this.dateTime = LocalDateTime.now();
+    }
+
+    public Log(String logLevel, String path, String method, String requestData) {
+        this.dateTime = LocalDateTime.now();
+        this.logLevel = logLevel;
+        this.path = path;
+        this.method = method;
+        this.requestData = requestData;
+    }
+
+    @Override
+    public String toString() {
+        return dateTime + " " + logLevel + " Request " + path + " " + method + " " + requestData;
+    }
 }
