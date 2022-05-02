39700aa910701bf3349cb1dd232b9c399bc2da48
tminus109 <martontibor123@gmail.com>
2022-05-02 13:31:46 +0200

    cleanup: deleted p2p project

--- p2p_chat/src/main/java/com/gfa/p2p_chat/controllers/RestController.java
+++
@@ -1,4 +0,0 @@
-package com.gfa.p2p_chat.controllers;
-
-public class RestController {
-}
--- p2p_chat/src/main/java/com/gfa/p2p_chat/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gfa.p2p_chat.repositories;
-
-import com.gfa.p2p_chat.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsernameIgnoreCase(String username);
-}
