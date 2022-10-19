908725c9d16e5400e28092f51e7c34060448099d
tminus109 <martontibor123@gmail.com>
2022-10-19 16:25:47 +0200

    cleanup: renamed folds to foldCount, arr to foldedArray, input to array

--- codewars/exercises/fold_an_array.md
+++ codewars/exercises/fold_an_array.md
@@ -1,27 +1,14 @@
-In this kata you have to write a method that folds a given array of integers
-by the middle x-times.
-
-An example says more than a thousand words:
+In this kata you have to write a method that folds a given array of integers by the middle x-times.
 
 Fold 1-time:
 
 [1,2,3,4,5] -> [6,6,3]
 
-A little visualization (NOT for the algorithm but for the idea of folding):
-
-Step 1         Step 2        Step 3       Step 4       Step5
-
-5/           5|         5\          
-4/            4|          4\      
-1 2 3 4 5      1 2 3/         1 2 3|       1 2 3\       6 6 3
-----*----      ----*          ----*        ----*        ----*
-
-
 Fold 2-times:
 
 [1,2,3,4,5] -> [9,6]
 
-As you see, if the count of numbers is odd, the middle number will stay.
+If the count of numbers is odd, the middle number will stay.
 Otherwise, the fold-point is between the middle-numbers, so all numbers would be added in a way.
 The array will always contain numbers and will never be null.
 The parameter runs will always be a positive integer greater than 0
--- codewars/src/FoldAnArray.java
+++ codewars/src/FoldAnArray.java
@@ -1,24 +1,22 @@
 import java.util.Arrays;
 
 public class FoldAnArray {
-    public static int[] foldArray(int[] array, int folds) {
-        int[] arr = Arrays.copyOf(array, array.length);
-        int length = arr.length;
-        for (int i = 0; i < folds; i++) {
+    public static int[] foldArray(int[] array, int foldCount) {
+        int[] foldedArray = Arrays.copyOf(array, array.length);
+        int length = foldedArray.length;
+        for (int i = 0; i < foldCount; i++) {
             for (int j = 0; j < length / 2; j++) {
-                arr[j] += arr[length - 1 - j];
+                foldedArray[j] += foldedArray[length - 1 - j];
             }
             length = (length + 1) / 2;
         }
-        return Arrays.copyOf(arr, length);
+        return Arrays.copyOf(foldedArray, length);
     }
 
     public static void main(String[] args) {
-        int[] input = {1, 2, 3, 4, 5};
-        int fold = 1;
-        int fold2 = 2;
+        int[] array = {1, 2, 3, 4, 5};
 
-        System.out.println(Arrays.toString(foldArray(input, fold)));
-        System.out.println(Arrays.toString(foldArray(input, fold2)));
+        System.out.println(Arrays.toString(foldArray(array, 1)));
+        System.out.println(Arrays.toString(foldArray(array, 2)));
     }
 }
