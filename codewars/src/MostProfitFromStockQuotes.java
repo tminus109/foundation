e093550a59e57676d3f3c0e16d493180e211a841
tminus109 <martontibor123@gmail.com>
2022-10-25 13:28:03 +0200

    fix: i < quotes.length

--- codewars/src/MostProfitFromStockQuotes.java
+++ codewars/src/MostProfitFromStockQuotes.java
@@ -1,36 +1,33 @@
 public class MostProfitFromStockQuotes {
-    public static int mostProfit(int[] quotes) {
-        int maxProfit = 0;
-        int stock = 0;
+    public static int countMostProfit(int[] quotes) {
+        int profit = 0;
+        int stockCount = 0;
 
-        for (int i = 0; i < quotes.length - 1; i++) {
+        for (int i = 0; i < quotes.length; i++) {
             int bestPrice = quotes[i];
+
             for (int j = (i + 1); j < quotes.length; j++) {
                 if (quotes[j] > bestPrice) {
                     bestPrice = quotes[j];
                 }
             }
+
             if (bestPrice > quotes[i]) {
-                stock++;
-                maxProfit -= quotes[i];
+                stockCount++;
+                profit -= quotes[i];
             } else if (bestPrice == quotes[i]) {
-                maxProfit += stock * bestPrice;
-                stock = 0;
+                profit += stockCount * bestPrice;
+                stockCount = 0;
             }
         }
 
-        return maxProfit;
+        return profit;
     }
 
     public static void main(String[] args) {
-        int[] quotes1 = {1, 2, 3, 4, 5, 6};
-        int[] quotes2 = {6, 5, 4, 3, 2, 1};
-        int[] quotes3 = {1, 6, 5, 10, 8, 7};
-        int[] quotes4 = {1, 2, 10, 3, 2, 7, 3, 2};
-
-        System.out.println(mostProfit(quotes1));
-        System.out.println(mostProfit(quotes2));
-        System.out.println(mostProfit(quotes3));
-        System.out.println(mostProfit(quotes4));
+        System.out.println(countMostProfit(new int[]{1, 2, 3, 4, 5, 6}));
+        System.out.println(countMostProfit(new int[]{6, 5, 4, 3, 2, 1}));
+        System.out.println(countMostProfit(new int[]{1, 6, 5, 10, 8, 7}));
+        System.out.println(countMostProfit(new int[]{1, 2, 10, 3, 2, 7, 3, 2}));
     }
 }
