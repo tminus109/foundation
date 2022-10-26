e7f72ff9751c1471784836b73fc954172b806ff3
tminus109 <martontibor123@gmail.com>
2022-10-26 20:10:51 +0200

    refact: improved performance of for loop

--- hacker_rank/exercises/birthday_cake_candles.md
+++ hacker_rank/exercises/birthday_cake_candles.md
@@ -4,7 +4,9 @@ They will only be able to blow out the tallest of the candles.
 Count how many candles are tallest.
 
 Example:
+
 candles = [4, 4, 1, 3]
+
 The maximum height candles are 4 units high. There are 2 of them, so return 2.
 
 Function Description: complete the function birthdayCakeCandles.
@@ -14,19 +16,19 @@ birthdayCakeCandles has the following parameter(s): int candles[n], the candle h
 Returns int: the number of candles that are tallest.
 
 Input Format:
+
 The first line contains a single integer, n, the size of candles[].
 The second line contains n space-separated integers,
 where each integer i describes the height of candles[i].
 
 Constraints:
+
 1 <= n <= 10⁵
+
 1 <= candles[i] <= 10⁷
 
-Sample Input 0:
-4
-3 2 1 3
+Sample Input: [4 3 2 1 3]
 
-Sample Output 0:
-2
+Sample Output: 2
 
 https://www.hackerrank.com/challenges/birthday-cake-candles/problem
--- hacker_rank/src/BirthdayCakeCandles.java
+++ hacker_rank/src/BirthdayCakeCandles.java
@@ -1,25 +1,25 @@
 public class BirthdayCakeCandles {
-    public static int birthdayCakeCandles(int[] candles) {
-        int tallestCandlesCount = 0;
-        int height = 0;
-        for (int candle : candles) {
-            if (candle > height) {
-                height = candle;
-                tallestCandlesCount = 1;
-            } else if (candle == height) {
-                tallestCandlesCount++;
+    public static int countTallestCandles(int[] candles) {
+        int tallestCandleCount = 1;
+        int tallestCandle = candles[0];
+
+        for (int i = 1, l = candles.length; i < l; i++) {
+            int candle = candles[i];
+            if (candle > tallestCandle) {
+                tallestCandle = candle;
+                tallestCandleCount = 1;
+            } else if (candle == tallestCandle) {
+                tallestCandleCount++;
             }
         }
-        return tallestCandlesCount;
+
+        return tallestCandleCount;
     }
 
     public static void main(String[] args) {
-        int[] candles0 = {4, 4, 1, 3};
-        int[] candles1 = {3, 2, 1, 3};
-        int[] candles2 = {3, 3, 3, 4, 5, 7, 6, 4, 2, 8, 4, 4, 8, 8, 9, 8};
-
-        System.out.println(birthdayCakeCandles(candles0));
-        System.out.println(birthdayCakeCandles(candles1));
-        System.out.println(birthdayCakeCandles(candles2));
+        System.out.println(countTallestCandles(new int[]{4, 4, 1, 3}));
+        System.out.println(countTallestCandles(new int[]{3, 2, 1, 3}));
+        System.out.println(countTallestCandles(new int[]{3, 3, 3, 4, 5, 7, 6, 4, 2, 8, 4, 4, 8, 8, 8}));
+        System.out.println(countTallestCandles(new int[]{3, 3, 3, 4, 5, 7, 6, 4, 2, 8, 4, 4, 8, 9, 8}));
     }
 }
