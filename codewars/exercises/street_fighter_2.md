f02cc2e9417ce174e3dbc070a043c7fb48b6b86a
tminus109 <martontibor123@gmail.com>
2022-10-26 10:35:44 +0200

    cleanup: renamed variables

--- codewars/exercises/street_fighter_2.md
+++ codewars/exercises/street_fighter_2.md
@@ -1,21 +1,24 @@
 You'll have to simulate the video game's character selection screen behaviour,
 more specifically the selection grid.
 
-Selection Grid Layout in text:
+Selection grid layout in text:
 
 | Ryu  | E.Honda | Blanka  | Guile   | Balrog | Vega    |
 | Ken  | Chun Li | Zangief | Dhalsim | Sagat  | M.Bison |
 
 Input:
+
 The list of game characters in a 2x6 grid.
 The initial position of the selection cursor (top-left is (0,0)).
 A list of moves of the selection cursor (which are up, down, left, right).
 
 Output:
+
 The list of characters who have been hovered by the selection cursor after all the moves
 (ordered and with repetition, all the ones after a move, weather successful or not, see tests).
 
 Rules:
+
 Selection cursor is circular horizontally but not vertically!
 As you might remember from the game, the selection cursor rotates horizontally
 but not vertically; that means that if I'm in the leftmost, and I try to go left again
@@ -28,6 +31,7 @@ You can't go upper than highest row:
 Ryu, E.Honda, Blanka, Guile, Balrog and Vega in the above image).
 
 Test:
+
 For this easy version the fighters grid layout and the initial position will always be
 the same in all tests, only the list of moves change.
 Notice: changing some of the input data might not help you.
@@ -35,6 +39,7 @@ Notice: changing some of the input data might not help you.
 Examples:
 
 1.
+
 fighters = [
 ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
 ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
@@ -43,11 +48,14 @@ initial_position = (0,0)
 moves = ['up', 'left', 'right', 'left', 'left']
 
 Result:
+
 ['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']
-- the characters I've been hovering over with the selection cursor during my moves.
+
+The characters I've been hovering over with the selection cursor during my moves.
 Notice: Ryu is the first just because it "fails" the first up. See test cases for more examples.
 
 2.
+
 fighters = [
 ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
 ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
@@ -56,6 +64,7 @@ initial_position = (0,0)
 moves = ['right', 'down', 'left', 'left', 'left', 'left', 'right']
 
 Result:
+
 ['E.Honda', 'Chun Li', 'Ken', 'M.Bison', 'Sagat', 'Dhalsim', 'Sagat']
 
 https://www.codewars.com/kata/5853213063adbd1b9b0000be
--- codewars/src/StreetFighter2.java
+++ codewars/src/StreetFighter2.java
@@ -2,32 +2,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StreetFighter2 {
-    public static List<String> getCharacters(String[][] fighters, String[] moves) {
-        List<String> characters = new ArrayList<>();
-        int horizontalPosition = 0;
-        int verticalPosition = 0;
+    public static List<String> getHoveredCharacters(String[][] fighters, String[] moves) {
+        List<String> hoveredCharacters = new ArrayList<>();
+        int horPos = 0;
+        int vertPos = 0;
 
         for (String move : moves) {
             if (move.equals("left")) {
-                if (horizontalPosition == 0) {
-                    horizontalPosition = 5;
+                if (horPos == 0) {
+                    horPos = 5;
                 } else {
-                    horizontalPosition--;
+                    horPos--;
                 }
             } else if (move.equals("right")) {
-                if (horizontalPosition == 5) {
-                    horizontalPosition = 0;
+                if (horPos == 5) {
+                    horPos = 0;
                 } else {
-                    horizontalPosition++;
+                    horPos++;
                 }
-            } else if (move.equals("up") && verticalPosition == 1) {
-                verticalPosition = 0;
-            } else if (move.equals("down") && verticalPosition == 0)
-                verticalPosition = 1;
-            characters.add(fighters[verticalPosition][horizontalPosition]);
+            } else if (move.equals("up") && vertPos == 1) {
+                vertPos = 0;
+            } else if (move.equals("down") && vertPos == 0) {
+                vertPos = 1;
+            }
+
+            hoveredCharacters.add(fighters[vertPos][horPos]);
         }
 
-        return characters;
+        return hoveredCharacters;
     }
 
     public static void main(String[] args) {
@@ -35,10 +37,10 @@ public class StreetFighter2 {
                 {"Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"},
                 {"Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"}
         };
-        String[] moves0 = {"up", "left", "right", "left", "left"};
+        String[] moves = {"up", "left", "right", "left", "left"};
         String[] moves1 = {"right", "down", "left", "left", "left", "left", "right"};
 
-        System.out.println(getCharacters(fighters, moves0));
-        System.out.println(getCharacters(fighters, moves1));
+        System.out.println(getHoveredCharacters(fighters, moves));
+        System.out.println(getHoveredCharacters(fighters, moves1));
     }
 }
