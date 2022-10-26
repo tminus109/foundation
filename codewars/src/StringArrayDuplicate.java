6b2f0db8e6b5b7b36c5651cbb1e927fda5620eba
tminus109 <martontibor123@gmail.com>
2022-10-26 11:12:39 +0200

    cleanup: renamed variables

--- codewars/src/StringArrayDuplicate.java
+++ codewars/src/StringArrayDuplicate.java
@@ -1,27 +1,29 @@
 import java.util.Arrays;
 
 public class StringArrayDuplicate {
-    public static String[] removeDuplicateLetters(String[] arr) {
-        String[] output = new String[arr.length];
+    public static String[] removeConsecutiveDuplicates(String[] arr) {
+        String[] noDuplicates = new String[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
             StringBuilder sb = new StringBuilder(arr[i]);
+
             for (int j = 1; j < sb.length(); j++) {
                 if (sb.charAt(j) == sb.charAt(j - 1)) {
                     sb.deleteCharAt(j);
                 }
             }
-            output[i] = sb.toString();
+
+            noDuplicates[i] = sb.toString();
         }
 
-        return output;
+        return noDuplicates;
     }
 
     public static void main(String[] args) {
-        String[] arr0 = {"abracadabra", "allottee", "assessee"};
-        String[] arr1 = {"kelless", "keenness"};
+        String[] arr = {"abracadabra", "allottee", "assessee"};
+        String[] arr2 = {"kelless", "keenness"};
 
-        System.out.println(Arrays.toString(removeDuplicateLetters(arr0)));
-        System.out.println(Arrays.toString(removeDuplicateLetters(arr1)));
+        System.out.println(Arrays.toString(removeConsecutiveDuplicates(arr)));
+        System.out.println(Arrays.toString(removeConsecutiveDuplicates(arr2)));
     }
 }
