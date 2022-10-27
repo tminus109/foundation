37132bd3fee1f10d9a0ec024909bebe17f6698fe
tminus109 <martontibor123@gmail.com>
2022-10-27 09:47:58 +0200

    refact: simplified for loop logic

--- hacker_rank/src/DiagonalDifference.java
+++ hacker_rank/src/DiagonalDifference.java
@@ -1,35 +1,33 @@
 public class DiagonalDifference {
-    public static int diagonalDifference(int[][] arr) {
-        int leftToRightDiagonal = 0;
-        int rightToLeftDiagonal = 0;
+    public static int countDiagonalDiff(int[][] arr) {
+        int sumLeft = 0;
+        int sumRight = 0;
+        int length = arr.length;
+        int j = length - 1;
 
-        for (int i = 0; i < arr.length; i++) {
-            leftToRightDiagonal += arr[i][i];
-        }
-
-        int j = arr.length - 1;
-        for (int[] ints : arr) {
-            rightToLeftDiagonal += ints[j];
+        for (int i = 0; i < length; i++) {
+            sumLeft += arr[i][i];
+            sumRight += arr[i][j];
             j--;
         }
 
-        return Math.abs(leftToRightDiagonal - rightToLeftDiagonal);
+        return Math.abs(sumLeft - sumRight);
     }
 
     public static void main(String[] args) {
-        int[][] arr0 = {
+        int[][] arr = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {9, 8, 9}
         };
 
-        int[][] arr1 = {
+        int[][] arr2 = {
                 {11, 2, 4},
                 {4, 5, 6},
                 {10, 8, -12}
         };
 
-        System.out.println(diagonalDifference(arr0));
-        System.out.println(diagonalDifference(arr1));
+        System.out.println(countDiagonalDiff(arr));
+        System.out.println(countDiagonalDiff(arr2));
     }
 }
