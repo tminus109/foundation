ef7806e88e45cc3e0f390207bdcce756daaeec69
tminus109 <martontibor123@gmail.com>
2022-10-27 14:54:31 +0200

    cleanup

--- hacker_rank/src/PlusMinus.java
+++ hacker_rank/src/PlusMinus.java
@@ -1,32 +1,30 @@
 import java.text.DecimalFormat;
 
 public class PlusMinus {
-    public static void plusMinus(int[] arr) {
+    public static void calcRatios(int[] arr) {
         DecimalFormat df = new DecimalFormat("0.000000");
-        int positiveNumbers = 0;
-        int negativeNumbers = 0;
-        int zeroes = 0;
+        int length = arr.length;
+        int posCount = 0;
+        int negCount = 0;
+        int zeroCount = 0;
 
-        for (int j : arr) {
-            if (j > 0) {
-                positiveNumbers++;
-            } else if (j < 0) {
-                negativeNumbers++;
+        for (int n : arr) {
+            if (n > 0) {
+                posCount++;
+            } else if (n < 0) {
+                negCount++;
             } else {
-                zeroes++;
+                zeroCount++;
             }
         }
 
-        System.out.println(df.format((float) positiveNumbers / arr.length));
-        System.out.println(df.format((float) negativeNumbers / arr.length));
-        System.out.println(df.format((float) zeroes / arr.length));
+        System.out.println(df.format((float) posCount / length));
+        System.out.println(df.format((float) negCount / length));
+        System.out.println(df.format((float) zeroCount / length));
     }
 
     public static void main(String[] args) {
-        int[] arr0 = {1, 1, 0, -1, -1};
-        int[] arr1 = {-4, 3, -9, 0, 4, 1};
-
-        plusMinus(arr0);
-        plusMinus(arr1);
+        calcRatios(new int[]{1, 1, 0, -1, -1});
+        calcRatios(new int[]{-4, 3, -9, 0, 4, 1});
     }
 }
