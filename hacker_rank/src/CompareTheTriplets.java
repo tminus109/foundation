7cb25885b57574b88b071e16a2db94da2512db31
tminus109 <martontibor123@gmail.com>
2022-10-27 08:57:39 +0200

    cleanup

--- hacker_rank/src/CompareTheTriplets.java
+++ hacker_rank/src/CompareTheTriplets.java
@@ -3,32 +3,27 @@ import java.util.Arrays;
 public class CompareTheTriplets {
     public static int[] compareTriplets(int[] a, int[] b) {
         int[] scores = new int[2];
+        int length = a.length;
         int scoreA = 0;
         int scoreB = 0;
-        for (int i = 0; i < a.length; i++) {
+
+        for (int i = 0; i < length; i++) {
             if (a[i] > b[i]) {
                 scoreA++;
             } else if (a[i] < b[i]) {
                 scoreB++;
             }
         }
+
         scores[0] = scoreA;
         scores[1] = scoreB;
+
         return scores;
     }
 
     public static void main(String[] args) {
-        int[] a0 = {5, 6, 7};
-        int[] b0 = {3, 6, 10};
-
-        int[] a1 = {17, 28, 30};
-        int[] b1 = {99, 16, 8};
-
-        int[] a2 = {1, 2, 3};
-        int[] b2 = {3, 2, 1};
-
-        System.out.println(Arrays.toString(compareTriplets(a0, b0)));
-        System.out.println(Arrays.toString(compareTriplets(a1, b1)));
-        System.out.println(Arrays.toString(compareTriplets(a2, b2)));
+        System.out.println(Arrays.toString(compareTriplets(new int[]{5, 6, 7}, new int[]{3, 6, 10})));
+        System.out.println(Arrays.toString(compareTriplets(new int[]{17, 28, 30}, new int[]{99, 16, 8})));
+        System.out.println(Arrays.toString(compareTriplets(new int[]{1, 2, 3}, new int[]{3, 2, 1})));
     }
 }
