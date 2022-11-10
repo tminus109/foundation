aa89be558f35822ccaec29bbd87a4c0ec5aaf1dd
tminus109 <martontibor123@gmail.com>
2022-11-10 10:16:56 +0100

    refact: changed return type to long[]

--- codewars/src/PascalsTriangle.java
+++ codewars/src/PascalsTriangle.java
@@ -1,35 +1,38 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PascalsTriangle {
-    public static List<Integer> getPascalsTriangle(int depth, int firstNum) {
-        if (depth < 1) {
-            return new ArrayList<>();
-        }
-
-        List<Integer> triangle = new ArrayList<>();
+    public static long[] getPascalsTriangle(int level) {
+        List<Long> numbers = new ArrayList<>();
         int rowLength = 1;
 
-        for (int i = 0; i < depth; i++) {
+        for (int i = 0; i < level; i++) {
             for (int j = 0; j < rowLength; j++) {
                 if (j == 0 || j == (rowLength - 1)) {
-                    triangle.add(firstNum);
+                    numbers.add(1L);
                 } else {
-                    triangle.add(
-                            triangle.get(triangle.size() - rowLength) +
-                                    triangle.get(triangle.size() - (rowLength - 1)));
+                    numbers.add(
+                            numbers.get(numbers.size() - rowLength) +
+                                    numbers.get(numbers.size() - (rowLength - 1)));
                 }
             }
             rowLength++;
+        }
+
+        long[] triangle = new long[numbers.size()];
+
+        for (int i = 0; i < numbers.size(); i++) {
+            triangle[i] = numbers.get(i);
         }
 
         return triangle;
     }
 
     public static void main(String[] args) {
-        System.out.println(getPascalsTriangle(1, 1));
-        System.out.println(getPascalsTriangle(4, 1));
-        System.out.println(getPascalsTriangle(6, 2));
-        System.out.println(getPascalsTriangle(0, 1));
+        System.out.println(Arrays.toString(getPascalsTriangle(1)));
+        System.out.println(Arrays.toString(getPascalsTriangle(4)));
+        System.out.println(Arrays.toString(getPascalsTriangle(6)));
+        System.out.println(Arrays.toString(getPascalsTriangle(0)));
     }
 }
