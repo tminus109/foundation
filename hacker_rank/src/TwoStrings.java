4d89c5dea7ce6fada6e7535ccea11c5bff5942a8
tminus109 <martontibor123@gmail.com>
2022-11-14 21:22:49 +0100

    refact: optimized solution by using Sets

--- hacker_rank/src/TwoStrings.java
+++ hacker_rank/src/TwoStrings.java
@@ -1,10 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class TwoStrings {
     public static String compareStrings(String s1, String s2) {
-        for (int i = 0; i < s1.length(); i++) {
-            if (s2.contains(String.valueOf(s1.charAt(i)))) {
-                return "YES";
-            }
+        Set<Character> s1Set = new HashSet<>();
+        Set<Character> s2Set = new HashSet<>();
+
+        for (char c : s1.toCharArray()) {
+            s1Set.add(c);
         }
+
+        for (char c : s2.toCharArray()) {
+            s2Set.add(c);
+        }
+
+        s1Set.retainAll(s2Set);
+
+        if (!s1Set.isEmpty())
+            return "YES";
 
         return "NO";
     }
